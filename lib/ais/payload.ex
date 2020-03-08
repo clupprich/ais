aaea909df9cbac113631f24e12955ece2ea24167
Dashie <dashie@sigpipe.me>
2020-03-08 10:22:22 +0100

    Revert "Refactoring of pattern match of payloads"
    This reverts commit c6961023cfca373e637335ac4708118b65d5cf4e.

--- lib/ais/payload.ex
+++ lib/ais/payload.ex
@@ -1,7 +1,8 @@
 defmodule AIS.Payload do
   def parse(payload) do
-    <<message_id::6, tail::bitstring>> = SixBit.decode(payload)
+    payload = SixBit.decode(payload)
 
+    <<message_id::6, tail::bitstring>> = payload
     attributes = parse_message(message_id, tail)
 
     Map.merge(%{message_id: message_id}, attributes)
@@ -11,14 +12,13 @@ defmodule AIS.Payload do
   # https://www.navcen.uscg.gov/?pageName=AISMessagesA
   # !AIVDM,1,1,,B,13IbQQ000100lq`LD7J6Vi<n88AM,0*52  ID 1
   # !AIVDM,1,1,,B,39NSDjP02201T0HLBJDBv2GD02s1,0*14  ID 3
-  defp parse_message(
-         message_id,
-         <<repeat_indicator::2, user_id::30, navigational_status::4, rate_of_turn::8, sog::10,
-           position_acucuracy::1, longitude::28, latitude::27, cog::12, true_heading::9,
-           time_stamp::6, special_manoeuvre_indicator::2, spare::3, raim_flag::1,
-           communication_state::19, _::bitstring>>
-       )
+  defp parse_message(message_id, payload)
        when message_id == 1 or message_id == 2 or message_id == 3 do
+    <<repeat_indicator::2, user_id::30, navigational_status::4, rate_of_turn::8, sog::10,
+      position_acucuracy::1, longitude::28, latitude::27, cog::12, true_heading::9, time_stamp::6,
+      special_manoeuvre_indicator::2, spare::3, raim_flag::1, communication_state::19,
+      _::bitstring>> = payload
+
     %{
       repeat_indicator: repeat_indicator,
       user_id: user_id,
@@ -41,15 +41,13 @@ defmodule AIS.Payload do
   # AIS Base Station Report (Message 4) And Coordinated Universal Time And Date Response (Message 11)
   # https://www.navcen.uscg.gov/?pageName=AIS_Base_Station_Report
   # !AIVDM,1,1,,A,402;bFQv@kkLc00Dl4LE52100@J6,0*58  ID 4
-  defp parse_message(
-         message_id,
-         <<repeat_indicator::2, user_id::30, utc_year::14, utc_month::4, utc_day::5, utc_hour::5,
-           utc_minute::6, utc_second::6, position_accuracy::1, longitude::28, latitude::27,
-           type_of_electronic_position_fixing_device::4,
-           transmission_control_for_long_range_broadcast_message::1, spare::9, raim_flag::1,
-           communication_state::19>>
-       )
-       when message_id == 4 or message_id == 11 do
+  defp parse_message(message_id, payload) when message_id == 4 or message_id == 11 do
+    <<repeat_indicator::2, user_id::30, utc_year::14, utc_month::4, utc_day::5, utc_hour::5,
+      utc_minute::6, utc_second::6, position_accuracy::1, longitude::28, latitude::27,
+      type_of_electronic_position_fixing_device::4,
+      transmission_control_for_long_range_broadcast_message::1, spare::9, raim_flag::1,
+      communication_state::19>> = payload
+
     %{
       repeat_indicator: repeat_indicator,
       user_id: user_id,
@@ -73,15 +71,13 @@ defmodule AIS.Payload do
 
   # AIS Class A Ship Static And Voyage Related Data (Message 5)
   # https://www.navcen.uscg.gov/?pageName=AISMessagesAStatic
-  defp parse_message(
-         message_id,
-         <<repeat_indicator::2, user_id::30, ais_version_indicator::2, imo_number::30,
-           call_sign::42, name::120, type_of_ship_and_cargo_type::8, dimension_a::9,
-           dimension_b::9, dimension_c::6, dimension_d::6,
-           type_of_electronic_position_fixing_devise::4, eta::20,
-           maximum_present_static_draught::8, destination::120, dte::1, spare::1, _::bitstring>>
-       )
-       when message_id == 5 do
+  defp parse_message(message_id, payload) when message_id == 5 do
+    <<repeat_indicator::2, user_id::30, ais_version_indicator::2, imo_number::30, call_sign::42,
+      name::120, type_of_ship_and_cargo_type::8, dimension_a::9, dimension_b::9, dimension_c::6,
+      dimension_d::6, type_of_electronic_position_fixing_devise::4, eta::20,
+      maximum_present_static_draught::8, destination::120, dte::1, spare::1,
+      _::bitstring>> = payload
+
     %{
       repeat_indicator: repeat_indicator,
       user_id: user_id,
@@ -107,12 +103,10 @@ defmodule AIS.Payload do
   # AIS Binary Addressed Message (Message 6)
   # https://www.navcen.uscg.gov/?pageName=AISMessage6
   # !AIVDM,1,1,,A,6>jCKIkfJjOt>db;q700@20,2*16
-  defp parse_message(
-         message_id,
-         <<repeat_indicator::2, source_id::30, sequence_number::2, destination_id::30,
-           retransmit_flag::1, spare::1, _::bitstring>>
-       )
-       when message_id == 6 do
+  defp parse_message(message_id, payload) when message_id == 6 do
+    <<repeat_indicator::2, source_id::30, sequence_number::2, destination_id::30,
+      retransmit_flag::1, spare::1, _::bitstring>> = payload
+
     # Binary data = max 936 bits
     # TODO: how to handle that ?
     %{
@@ -128,8 +122,9 @@ defmodule AIS.Payload do
   # AIS Binary Acknowledgment Message (Message 7)
   # https://gpsd.gitlab.io/gpsd/AIVDM.html#_type_7_binary_acknowledge
   # !AIVDM,1,1,,A,777QkG00RW38,0*62
-  defp parse_message(message_id, <<repeat_indicator::2, source_id::30, spare::2, _::bitstring>>)
-       when message_id == 7 do
+  defp parse_message(message_id, payload) when message_id == 7 do
+    <<repeat_indicator::2, source_id::30, spare::2, _::bitstring>> = payload
+
     %{
       repeat_indicator: repeat_indicator,
       source_id: source_id,
@@ -140,12 +135,10 @@ defmodule AIS.Payload do
   # AIS Binary Broadcast Message (Message 8)
   # https://www.navcen.uscg.gov/?pageName=AISMessage8
   # !AIVDM,1,1,,A,83HT5APj2P00000001BQJ@2E0000,0*72
-  defp parse_message(
-         message_id,
-         <<repeat_indicator::2, source_id::30, spare::2, designated_area_code::10,
-           functional_id::6, data::bitstring>>
-       )
-       when message_id == 8 do
+  defp parse_message(message_id, payload) when message_id == 8 do
+    <<repeat_indicator::2, source_id::30, spare::2, designated_area_code::10, functional_id::6,
+      data::bitstring>> = payload
+
     %{
       repeat_indicator: repeat_indicator,
       source_id: source_id,
@@ -191,11 +184,9 @@ defmodule AIS.Payload do
   # AIS COORDINATED UNIVERSAL TIME AND DATE INQUIRY (MESSAGE 10)
   # https://www.navcen.uscg.gov/?pageName=AIS_Base_Station_Report
   # !AIVDM,1,1,,A,:81:Jf1D02J0,0*0E
-  defp parse_message(
-         message_id,
-         <<repeat_indicator::2, source_id::30, spare1::2, destination_id::30, spare2::2>>
-       )
-       when message_id == 10 do
+  defp parse_message(message_id, payload) when message_id == 10 do
+    <<repeat_indicator::2, source_id::30, spare1::2, destination_id::30, spare2::2>> = payload
+
     %{
       repeat_indicator: repeat_indicator,
       source_id: source_id,
@@ -251,15 +242,13 @@ defmodule AIS.Payload do
   # AIS Standard Class B Equipment Position Report (Message 18)
   # https://www.navcen.uscg.gov/?pageName=AISMessagesB
   # !AIVDM,1,1,,B,B3HOIj000H08MeW52k4F7wo5oP06,0*42
-  defp parse_message(
-         message_id,
-         <<repeat_indicator::2, user_id::30, spare1::8, sog::10, position_accuracy::1,
-           longitude::28, latitude::27, cog::12, true_heading::9, time_stamp::6, spare2::2,
-           class_b_unit_flag::1, class_b_display_flag::1, class_b_dsc_flag::1,
-           class_b_band_flag::1, class_b_message_22_flag::1, mode_flag::1, raim_flag::1,
-           communication_state_selector_flag::1, communication_state::19>>
-       )
-       when message_id == 18 do
+  defp parse_message(message_id, payload) when message_id == 18 do
+    <<repeat_indicator::2, user_id::30, spare1::8, sog::10, position_accuracy::1, longitude::28,
+      latitude::27, cog::12, true_heading::9, time_stamp::6, spare2::2, class_b_unit_flag::1,
+      class_b_display_flag::1, class_b_dsc_flag::1, class_b_band_flag::1,
+      class_b_message_22_flag::1, mode_flag::1, raim_flag::1,
+      communication_state_selector_flag::1, communication_state::19>> = payload
+
     %{
       repeat_indicator: repeat_indicator,
       user_id: user_id,
@@ -333,15 +322,13 @@ defmodule AIS.Payload do
   # AIS Aids To Navigation (ATON) Report (Message 21)
   # https://www.navcen.uscg.gov/?pageName=AISMessage21
   # !AIVDM,1,1,,B,E>jCfrv2`0c2h0W:0a2ah@@@@@@004WD>;2<H50hppN000,4*0A
-  defp parse_message(
-         message_id,
-         <<repeat_indicator::2, id::30, type_of_aids_to_navigation::5,
-           name_of_aids_to_navigation::120, position_accuracy::1, longitude::28, latitude::27,
-           dimension::30, type_of_electronic_position_fixing_device::4, time_stamp::6,
-           off_position_indicator::1, aton_status::8, raim_flag::1, virtual_aton_flag::1,
-           assigned_mode_flag::1, spare::1, _::bitstring>>
-       )
-       when message_id == 21 do
+  defp parse_message(message_id, payload) when message_id == 21 do
+    <<repeat_indicator::2, id::30, type_of_aids_to_navigation::5, name_of_aids_to_navigation::120,
+      position_accuracy::1, longitude::28, latitude::27, dimension::30,
+      type_of_electronic_position_fixing_device::4, time_stamp::6, off_position_indicator::1,
+      aton_status::8, raim_flag::1, virtual_aton_flag::1, assigned_mode_flag::1, spare::1,
+      _::bitstring>> = payload
+
     # Extra dynamic fields:
     #  Name of Aid-to-Navigation Extension 	0, 6, 12, 18, 24, 30, 36, ... 84
     #  Spare 	0, 2, 4, or 6
